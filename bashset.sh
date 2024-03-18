07c716f99c2f0c67fa84f2394dfa5b6e8a6e5c0a
aut0-m8 <144866024+aut0-m8@users.noreply.github.com>
2024-03-18 17:44:17 +0000

    Update bashset.sh

--- bashset.sh
+++ bashset.sh
@@ -1,1 +1,9 @@
-echo "[-] check back later"
+echo "[-] script is in beta, check back later. contribute by making a pull request of this script. open repo?"
+read -p "[y/N]" yn
+
+case $yn in
+  y ) xdg-open https://github.com/aut0-m8/bashset;;
+  n ) exit 0;;
+  * ) exit 1;;
+esac
+exit 0;;
