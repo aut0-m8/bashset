e34945a84b391c055db741f7babd0f06bcf167d3
aut0-m8 <144866024+aut0-m8@users.noreply.github.com>
2024-03-18 17:11:06 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
 clear
-echo "[-] root is required to install pkgs"
-sudo echo "[-] password ok" \
+if sudo "echo" true 2>/dev/null; then # if sudo is currently active
+  :
+else
+  if sudo -V true 2>/dev/null; then # if sudo exists
+    echo "[-] root is required to install pkgs"
+    sudo echo "[-] password ok"
+  else
+   echo "[-] root is required to install pkgs"
+   su -c " echo '[-] password ok'"
+fi \
+
+
 && curl -H 'Pragma: no-cache' -sSL -o -bashset.sh https://raw.githubusercontent.com/aut0-m8/bashset/main/bashset.sh \
 && sudo chmod +x bashset.sh \
 && ./bashset.sh
