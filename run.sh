b2b8018229fc9035c27bb7819243f481eb217e72
aut0-m8 <144866024+aut0-m8@users.noreply.github.com>
2024-03-17 23:46:35 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 clear
 echo "[!] THIS SCRIPT CURRENTLY DOES NOT EXIST. CHECK BACK LATER."
 echo "[-] root is required to install pkgs"
-sudo curl -sSL https://raw.githubusercontent.com/aut0-m8/bashset/main/bashset.sh | bash;
+sudo curl -sSL -o bashset.sh https://raw.githubusercontent.com/aut0-m8/bashset/main/bashset.sh
+sudo chmod +x bashset.sh
+./bashset.sh
