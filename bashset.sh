016ed55c2ccd7d4cfae405ccc7999cfb03b6597e
aut0-m8 <144866024+aut0-m8@users.noreply.github.com>
2024-03-17 23:32:40 -0500

    Update bashset.sh

--- bashset.sh
+++ bashset.sh
@@ -1,3 +1,3 @@
 echo [!] THIS SCRIPT CURRENTLY DOES NOT EXIST. CHECK BACK LATER.
 echo [-] root is required to install pkgs
-sudo curl -sSL https://raw.githubusercontent.com/aut0-m8/bashset/main/bashset.sh | bash
+sudo curl -sSL https://raw.githubusercontent.com/aut0-m8/bashset/main/bashset.sh | bash;
