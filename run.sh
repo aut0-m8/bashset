f2ccee98b30fe9e3b6737ba85e06d03cf8db6c4c
aut0-m8 <144866024+aut0-m8@users.noreply.github.com>
2024-03-18 16:56:10 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 clear
 echo "[-] root is required to install pkgs"
-sudo echo "[-] password ok"
-curl -sSL -o bashset.sh https://raw.githubusercontent.com/aut0-m8/bashset/main/bashset.sh
-sudo chmod +x bashset.sh
-./bashset.sh
+sudo echo "[-] password ok" \
+&& curl -H 'Pragma: no-cache' -sSL -o -bashset.sh https://raw.githubusercontent.com/aut0-m8/bashset/main/bashset.sh \
+&& sudo chmod +x bashset.sh \
+&& ./bashset.sh
